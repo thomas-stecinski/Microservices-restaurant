e5eb2200b4bcd639c8505bd00ff6415f59668604
Saadct <92342579+Saadct@users.noreply.github.com>
2024-12-05 01:17:29 +0100

    add livraison service

---
+++ Livraison/Livraison/Livraison/Controllers/DeliveryController.cs
@@ -0,0 +1,57 @@
+﻿using Livraison.Models;
+using Livraison.Repository;
+using Livraison.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Livraison.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DeliveryController : ControllerBase
+    {
+        private readonly IDeliveryService _deliveryService;
+
+        public DeliveryController(IDeliveryService deliveryService)
+        {
+            _deliveryService = deliveryService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllDeliveries()
+        {
+            var deliveries = await _deliveryService.GetAllDeliveriesAsync();
+            return Ok(deliveries);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetDeliveryById(string id)
+        {
+            var delivery = await _deliveryService.GetDeliveryByIdAsync(id);
+            if (delivery == null)
+                return NotFound();
+            return Ok(delivery);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddDelivery(Delivery delivery)
+        {
+            await _deliveryService.AddDeliveryAsync(delivery);
+            return StatusCode(201, delivery);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateDelivery(string id, Delivery delivery)
+        {
+            await _deliveryService.UpdateDeliveryAsync(id, delivery);
+            return StatusCode(201, delivery);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteDelivery(string id)
+        {
+            await _deliveryService.DeleteDeliveryAsync(id);
+            return NoContent();
+        }
+    }
+}
---
+++ Livraison/Livraison/Livraison/Models/Delivery.cs
@@ -0,0 +1,19 @@
+﻿namespace Livraison.Models
+{
+    public class Delivery
+    {
+ 
+        public string Id { get; set; }
+        public string IdComande { get; set; }
+        public string IdClient { get; set; }
+        public string IdLivreur { get; set; }
+        public string Adresse { get; set; }
+        public string Ville { get; set; }
+        public string Status { get; set; }
+        public string Prix { get; set; }
+        public DateTime DateLivraison { get; set; }
+
+
+    }
+
+}
